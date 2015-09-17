23c58ed5dd399ef429a782f1c3cdb27aef76d9c3
Tina Liang <tina.tang.tl@gmail.com>
2015-09-17 13:24:22 -0400

    dom parser

--- src/Dom.java
+++ src/Dom.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -6,24 +7,6 @@ import org.w3c.dom.NodeList;
 
 
 public class Dom {
-	private String title;
-	private String author;
-	private int dimensionX;
-	private int dimensionY;
-	private ArrayList<String> colorList;
-	
-	public void handleVariables(Document document){
-		//sets up title and author strings to be called
-		title = document.getElementsByTagName("name").item(0).getAttributes().getNamedItem("title").toString();
-		author = document.getElementsByTagName("name").item(0).getAttributes().getNamedItem("author").toString();
-		
-		String tempX = document.getElementsByTagName("dimensionX").item(0).getTextContent();
-		String tempY = document.getElementsByTagName("dimensionY").item(0).getTextContent();
-		dimensionX = Integer.parseInt(tempX);
-		dimensionY = Integer.parseInt(tempY);
-		
-		colorList = handleColor(document);
-	}
 	
 	public ArrayList<String> handleColor(Document document){
 		ArrayList<String> colors = new ArrayList<String>();
@@ -36,6 +19,34 @@ public class Dom {
 			}
 		}
 		return colors;
+	}
+	
+	public String getTitle(Document document){
+		String title = document.getElementsByTagName("name").item(0).getAttributes().getNamedItem("title").toString();
+		return title;
+	}
+	
+	public String getAuthor(Document document){
+		String author = document.getElementsByTagName("name").item(0).getAttributes().getNamedItem("author").toString();
+		return author;
+	}
+	
+	public int getDimensionX(Document document){
+		String tempX = document.getElementsByTagName("dimensionX").item(0).getTextContent();
+		int dimensionX = Integer.parseInt(tempX);
+		return dimensionX;
+	}
+	
+	public int getDimensionY(Document document){
+		String tempY = document.getElementsByTagName("dimensionY").item(0).getTextContent();
+		int dimensionY = Integer.parseInt(tempY);
+		return dimensionY;
+	}
+	
+	public ArrayList<String> getColorList(Document document){
+		ArrayList<String> colorList = new ArrayList<String>();
+		colorList = handleColor(document);
+		return colorList;
 	}
 
 	
